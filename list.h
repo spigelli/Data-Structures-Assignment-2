d25d0298b93e3ca6746f7714ee3ba4c958bf685d
Isaac <isaacspiegel@gmail.com>
2024-02-29 16:48:55 -0600

    Add list headers

---
+++ list.h
@@ -0,0 +1,36 @@
+#include "node.h"
+
+// Header file for the List class
+template <typename Item_Type>
+class List
+{
+  private:
+    Node<Item_Type>* head;
+    Node<Item_Type>* tail;
+    int num_items;
+  public:
+    // Constructor
+    List();
+    // Destructor
+    ~List();
+    // Inserts a copy of item as the first element of the list.
+    void push_front(const Item_Type& item);
+    // Adds a copy of item to the end of the list.
+    void push_back(const Item_Type& item);
+    // Removes the first item from the list.
+    void pop_front();
+    // Removes the last item from the list.
+    void pop_back();
+    // Gets the first element in the list. Both constant and modifiable versions are provided.
+    Item_Type& front();
+    // Gets the last element in the list. Both constant and modifiable versions are provided.
+    Item_Type& back();
+    // Returns true if this list is empty.
+    bool empty() const;
+    // Insert item at position index (starting at 0). Insert at the end if index is beyond the end of the list.
+    void insert(size_t index, const Item_Type& item);
+    // Remove the item at position index. Return true if successful; return false if index is beyond the end of the list.
+    bool remove(size_t index);
+    // Return the position of the first occurrence of item if it is found. Return the size of the list if it is not found.
+    size_t find(const Item_Type& item);
+};
