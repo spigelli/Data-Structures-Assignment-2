80ee99bf58cdb9efc7b06a7b400b870872ad2f10
Isaac <isaacspiegel@gmail.com>
2024-02-29 16:39:48 -0600

    Add node class headers

---
+++ node.h
@@ -0,0 +1,20 @@
+// Header file for the node class which is generic
+
+template <typename Item_Type>
+class Node {
+  private:
+    // Data members
+    Item_Type data;
+    Node* next;
+  public:
+    // Constructor
+    Node(Item_Type data, Node* next);
+    // Destructor
+    ~Node()
+    // Getters and setters
+    Item_Type& getData()
+    void 
+};
+
+    
+
