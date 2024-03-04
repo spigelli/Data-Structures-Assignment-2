8b440e3ca3c1c50ebf678b778cb69ec72eec9af3
Isaac <isaacspiegel@gmail.com>
2024-03-04 10:02:04 -0600

    Add test suite mechanism

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
+#include "list.h"
+#include "test.h"
 #include <iostream>
 
+void test_testing() {
+  TestSuite suite("Testing suite tester");
+  // Anonymous function that will never throw
+  suite.addTest("Test that always passes", []() {});
+  // Anonymous function that will always throw
+  suite.addTest("Test that always fails", []() { throw std::runtime_error("This test always fails"); });
+  suite.run();
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  test_testing();
+  return 0;
 }
