b8248077612b898295932e8714058fe0886b3e69
Isaac <isaacspiegel@gmail.com>
2024-03-04 19:03:56 -0600

    Add tests

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-// #include "list.h"
-// #include "node.h"
 #include "list.cpp"
 #include "test.h"
 #include <iostream>
@@ -13,38 +11,6 @@ void test_testing() {
   suite.addTest("Test that always fails", []() { throw std::runtime_error("This test always fails"); });
   suite.run();
 }
-
-// void test_node() {
-//   TestSuite suite = TestSuite("Node");
-
-//   suite.addTest("Node constructor", []() {
-//     Node<int> node = Node<int>(1, nullptr);
-//   });
-
-//   suite.addTest("Node constructed with next", []() {
-//     Node<int>* next = new Node<int>(1, nullptr);
-//     Node<int>* node = new Node<int>(2, next);
-
-//     if (node->getNext() != next) {
-//       delete next;
-//       delete node;
-//       throw std::runtime_error("Node next is not the same as the one passed to the constructor");
-//     }
-
-//     delete next;
-//     delete node;
-//   });
-
-//   suite.addTest("Node data setter", []() {
-//     Node<int> node = Node<int>(1, nullptr);
-//     node.setData(2);
-//     if (node.getData() != 2) {
-//       throw std::runtime_error("Node data setter is not working");
-//     }
-//   });
-
-//   suite.run();
-// }
 
 List<int>* create_int_list() {
   int arr[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
@@ -65,10 +31,149 @@ void test_list() {
     cout << endl;
     delete list;
   });
+
+  suite.addTest("Push front", []() {
+    List<int>* list = new List<int>();
+    list->push_front(101);
+    list->push_front(202);
+    list->push_front(303);
+    cout << "List after push_front: ";
+    list->pretty_print();
+    cout << endl;
+    if (list->front() != 303) {
+      throw std::runtime_error("Push front failed");
+    }
+    int index_of_101 = list->find(101);
+    if (index_of_101 != 2) {
+      throw std::runtime_error(
+        "Expected 2, got " + std::to_string(index_of_101)
+      );
+    }
+    delete list;
+  });
+
+  suite.addTest("Push back", []() {
+    List<int>* list = new List<int>();
+    list->push_back(101);
+    list->push_back(202);
+    list->push_back(303);
+    cout << "List after push_back: ";
+    list->pretty_print();
+    cout << endl;
+    if (list->back() != 303) {
+      throw std::runtime_error("Push back failed");
+    }
+    int index_of_101 = list->find(101);
+    if (index_of_101 != 0) {
+      throw std::runtime_error(
+        "Expected 0, got " + std::to_string(index_of_101)
+      );
+    }
+    delete list;
+  });
+
+  suite.addTest("Pop front", []() {
+    List<int>* list = create_int_list();
+    cout << "List before pop_front: ";
+    list->pretty_print();
+    list->pop_front();
+    cout << "List after pop_front: ";
+    list->pretty_print();
+    cout << endl;
+    int list_front = list->front();
+    if (list_front != 18) {
+      throw std::runtime_error(
+        "Expected 18, got " + std::to_string(list_front)
+      );
+    }
+    delete list;
+  });
+
+  suite.addTest("Pop back", []() {
+    List<int>* list = create_int_list();
+    cout << "List before pop_back: ";
+    list->pretty_print();
+    list->pop_back();
+    cout << "List after pop_back: ";
+    list->pretty_print();
+    cout << endl;
+    int list_back = list->back();
+    if (list_back != 4) {
+      throw std::runtime_error(
+        "Expected 4, got " + std::to_string(list_back)
+      );
+    }
+    delete list;
+  });
+
+  suite.addTest("Empty", []() {
+    List<int>* list = new List<int>();
+    if (!list->empty()) {
+      throw std::runtime_error("List should be empty");
+    }
+    list->push_front(101);
+    if (list->empty()) {
+      throw std::runtime_error("List should not be empty");
+    }
+    delete list;
+  });
+
+  suite.addTest("Insert", []() {
+    List<int>* list = create_int_list();
+    cout << "List before insert: ";
+    list->pretty_print();
+    list->insert(3, 99);
+    cout << "List after insert: ";
+    list->pretty_print();
+    cout << endl;
+    int index_of_99 = list->find(99);
+    if (index_of_99 != 3) {
+      throw std::runtime_error(
+        "Expected 3, got " + std::to_string(index_of_99)
+      );
+    }
+    delete list;
+  });
+
+  suite.addTest("Remove", []() {
+    List<int>* list = create_int_list();
+    cout << "List before remove: ";
+    list->pretty_print();
+    list->remove(3);
+    cout << "List after remove: ";
+    list->pretty_print();
+    cout << endl;
+    int index_of_12 = list->find(12);
+    if (index_of_12 != 3) {
+      throw std::runtime_error(
+        "Expected 3, got " + std::to_string(index_of_12)
+      );
+    }
+    delete list;
+  });
+
+  suite.addTest("Find", []() {
+    List<int>* list = create_int_list();
+    int index_of_12 = list->find(12);
+    if (index_of_12 != 4) {
+      throw std::runtime_error(
+        "Expected 5, got " + std::to_string(index_of_12)
+      );
+    }
+    int index_of_99 = list->find(99);
+    if (index_of_99 != 10) {
+      throw std::runtime_error(
+        "Expected 10, got " + std::to_string(index_of_99)
+      );
+    }
+    delete list;
+  });
+
+  suite.run();
 }
 
 int main() {
   // test_testing();
-  // test_node();
+  test_list();
   return 0;
 }
